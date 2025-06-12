642ac8d0c7ac4c6b7f98fea7ef5ea5f94f6f5628
aditya <asadityasingh2572000@gmail.com>
2025-06-12 20:05:20 +0530

    adding hero Section

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
+
+      <div className='p-4 relative '></div>
+    </div>
+  )
+}
+
+export default HeroSection
