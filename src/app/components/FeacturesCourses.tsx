8c8d2de4b19cb505bfbf651086f472779188cf2d
reshminagle17 <reshminagle876@gmail.com>
2025-06-13 21:39:35 +0530

    complete f coueses

--- src/app/components/FeacturesCourses.tsx
+++ src/app/components/FeacturesCourses.tsx
@@ -30,8 +30,9 @@ const FeacturesCourses = () => {
         {fcourses.map((course:Course)=>(<div className='flex justify-center' key={course.id}>
           <BackgroundGradient className='flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm'>
             <div className='p-4 sm:p-6 flex flex-col items-center text-center flex-grow'>
-              <p>{course.title}</p>
-              <p>{course.description}</p>
+              <p className='text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200'>{course.title}</p>
+              <p className='text-sm text-neutral-600 dark:text-neutral-400 flex-grow'>{course.description}</p>
+              <Link href={`/course/${course.slug}`}>Lern More</Link>
             </div>
           </BackgroundGradient>
         </div>) )}
