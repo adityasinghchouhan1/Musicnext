88d55656c366d1cf033392b81fea41bbf7e98c31
reshminagle17 <reshminagle876@gmail.com>
2025-06-14 18:31:18 +0530

    start craeding Instructores section

---
+++ src/app/components/Instructores.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { WavyBackground } from './ui/wavy-background'
+
+const Instructores = () => {
+  return (
+    <>
+      <div className="h-[40rem] relative overflow-hidden flex items-center justify-center">
+        <WavyBackground></WavyBackground>
+      </div>
+    </>
+  )
+}
+
+export default Instructores
