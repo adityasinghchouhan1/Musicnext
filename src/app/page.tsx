f0f0d350e875b16c307fe652e702f31e47d53186
reshminagle17 <reshminagle876@gmail.com>
2025-06-14 18:42:43 +0530

    complete styling of instructor section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
-import FeacturesCourses from "./components/FeacturesCourses";
-import HeroSection from "./components/HeroSection";
-import MSchoolTesimonialCards from "./components/TesimonialCards";
-import UpcomingWebinars from "./components/UpcomingWebinars";
+import { Inspiration } from 'next/font/google'
+import FeacturesCourses from './components/FeacturesCourses'
+import HeroSection from './components/HeroSection'
+import MSchoolTesimonialCards from './components/TesimonialCards'
+import UpcomingWebinars from './components/UpcomingWebinars'
 import WhyChooseUs from './components/WhyChooseUs'
+import Instructores from './components/Instructores'
 
-export default function Home(){
-  return(
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">   
-    <HeroSection/>
-    <FeacturesCourses/>
-    <WhyChooseUs/>
-    <MSchoolTesimonialCards/>
-    <UpcomingWebinars/>
-</main>
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeacturesCourses />
+      <WhyChooseUs />
+      <MSchoolTesimonialCards />
+      <UpcomingWebinars />
+      <Instructores />
+    </main>
   )
 }
